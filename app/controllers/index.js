2424acd01a448faaa180142dd818d56ee67f467c
Kent <register@gividen.com>
2014-07-25 16:57:36 -0600

    Initial checkin. A place to put all spike solution tests for alloy.

---
+++ app/controllers/index.js
@@ -0,0 +1,5 @@
+$.index.open();
+
+function openTableView(){
+    Alloy.createController("tableView").getView().open();
+}
---
+++ app/models/Device.js
@@ -0,0 +1,43 @@
+exports.definition = {
+    config : {
+        "columns" : {
+            "sortId" : "INTEGER",
+            "name" : "TEXT",
+            "displayName" : "TEXT",
+            "address" : "TEXT",
+            "showInView" : "INTEGER",
+            "type" : "TEXT",
+            "parent" : "TEXT"
+        },
+        "defaults" : {
+            "sortId" : 0,
+            "name" : "",
+            "displayName" : "",
+            "address" : "",
+            "showInView" : 0,
+            "type" : "unknown",
+            "parent" : "unknown"
+        },
+        "adapter" : {
+            "type" : "sql",
+            "collection_name" : "devices"
+        }
+    },
+
+    extendModel : function(Model) {
+        _.extend(Model.prototype, {
+
+        });
+
+        return Model;
+    },
+
+    extendCollection : function(Collection) {
+        _.extend(Collection.prototype, {
+            comparator: function(collection) {
+                return collection.get('sortId');
+            }
+        });
+        return Collection;
+    }
+};

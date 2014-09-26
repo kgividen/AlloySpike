ef1cb636b52761dda98a87b880adba3732eb7864
Kent <register@gividen.com>
2014-09-26 09:13:08 -0600

    Added countdown widget

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,6 +15,15 @@ function openCountDown(){
     Alloy.createController("countdown").getView().open();
 }
 
+var countSeconds = 6,   //How many seconds to countdown.
+    endMsg = "Start!";  //What message to display after countdown is finished.
+
+$.countdownWidget.init(countSeconds,endMsg);
+
+function openCountDownWidget(){
+    $.countdownWidget.start();
+}
+
 function openLinkInStore(){
     Ti.API.info("open!!") ;
     Ti.Platform.openURL('itms://itunes.apple.com/us/app/fun-memory-game/id881939773?mt=8&uo=4');
@@ -25,7 +34,7 @@ var appId = "APPID",   //App ID for itunes or GooglePlay
     appName = "CoolApp", //Name of you APP for the Title
     appMsg = "If you enjoy using CoolApp, would you mind taking a moment to rate it?  Thanks!",//Message you want to display to the user to get them to rate it.
     runEvery = 3, //How many times can they run the app before being prompted to rate it.
-    appDebug = false; //If set to true it will run it every time.
+    appDebug = true; //If set to true it will run it every time.
 $.rating.init(appId, appName, appMsg, runEvery, appDebug);
 
 function openRatingWidget(){
